95ad2e05e7c1dc28f9fd9f7030e334d93cfeb509
Manuel Fuchs <manuel.fuchs49@gmail.com>
2018-04-03 23:48:19 +0200

    Alter namespace

--- BrickSolution.Logic/Constants.cs
+++ BrickSolution.Logic/Constants.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MonoBrickTest
+namespace BrickSolution.Logic
 {
     internal static class Constants
     {
--- BrickSolution.Logic/Robot.cs
+++ BrickSolution.Logic/Robot.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace MonoBrickTest
+namespace BrickSolution.Logic
 {
     public class Robot
     {
