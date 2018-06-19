f68d5e093b72c3e21e5b19126ef0dacfee6bde86
Manuel Fuchs <manuel.fuchs49@gmail.com>
2018-06-19 10:32:58 +0200

    Implement distance reader

--- BrickSolution.Logic/Program.cs
+++ BrickSolution.Logic/Program.cs
@@ -16,78 +16,13 @@ namespace BrickSolution.Logic
                 Thread.Sleep(1000);
 
                 Robot.Print("initialization succeeded");
-                
-                while (true)
+
+                for (int i = 0; i < 10; i++)
                 {
-                    Robot.Drive();
+                    Wait();
 
-                    Robot.Print($"last-stop-reason: {Robot.LastStopReason}");
-                    Thread.Sleep(7000);
-
-                    switch (Robot.LastStopReason)
-                    {
-                        case StopReason.AbyssDetected:
-                            Robot.Rotate(
-                                RotationMode.OtherMode,
-                                Constants.ROTATION_SPEED_FORWARD,
-                                Constants.ROTATION_SPEED_BACKWARD);
-                            break;
-                        case StopReason.ObstacleDetected:
-                            Robot.Rotate(
-                                RotationMode.OtherMode,
-                                Constants.ROTATION_SPEED_FORWARD,
-                                Constants.ROTATION_SPEED_BACKWARD);
-                            break;
-                        case StopReason.FenceDetected:
-                            if (Robot.TeamMode == TeamMode.IAhTeam)
-                            {
-                                Robot.PushObjectInFrontToLeft();
-                            }
-                            else
-                            {
-                                Robot.Rotate(
-                                    RotationMode.TimerMode,
-                                    Constants.ROTATION_SPEED_FORWARD,
-                                    Constants.ROTATION_SPEED_BACKWARD);
-                            }
-                            break;
-                        case StopReason.TreeDetected:
-                            Robot.Rotate(
-                                RotationMode.TimerMode,
-                                Constants.ROTATION_SPEED_FORWARD,
-                                Constants.ROTATION_SPEED_BACKWARD);
-                            break;
-                        case StopReason.OurFoodDetected:
-                            if (Robot.FoodState == FoodState.Searching)
-                            {
-                                Robot.CollectFood();
-                            }
-                            else
-                            {
-                                Robot.PushObjectInFrontToLeft();
-                            }
-                            
-                            break;
-                        case StopReason.EnemyFoodDetected:
-                            Robot.PushObjectInFrontToLeft();
-                            break;
-                        case StopReason.MeadowDetected:
-                            if (Robot.MeadowIsOurs()
-                                && Robot.FoodState == FoodState.Carrying)
-                            {
-                                Robot.RealeaseBrickOnMeadow();
-                            }
-                            else
-                            {
-                                Robot.Rotate(
-                                    RotationMode.HalfRotationMode,
-                                    Constants.ROTATION_SPEED_FORWARD,
-                                    Constants.ROTATION_SPEED_BACKWARD);
-                            }
-                            break;
-                        default:
-                            break;
-                    }
+                    Robot.Print($"us-dist: {Robot.GetUltraSonicDistance()}");
+                    Robot.Print($"ir-dist: {Robot.GetIRDistance()}");
                 }
             }
             catch (Exception e)
@@ -111,6 +46,30 @@ namespace BrickSolution.Logic
                 Robot.Print(Constants.PROGRAM_FINISHED_MSG);
                 Thread.Sleep(Constants.PROGRAM_ABORTION_DELAY);
             }
+        }
+
+        private static void Wait()
+        {
+            bool continueWithCompetition = false;
+
+            ButtonEvents btnEvents = new ButtonEvents();
+
+            Action btnAction = () => {
+                continueWithCompetition = true;
+            };
+
+            btnEvents.EnterPressed += btnAction;
+
+            Robot.PrintEmptyLine();
+            Robot.Print("press middle button to read");
+            Robot.Print("distance values");
+
+            while (!continueWithCompetition)
+            {
+                Thread.Sleep(Constants.SAMPLING_RATE);
+            }
+
+            btnEvents.EnterPressed -= btnAction;
         }
     }
 }
